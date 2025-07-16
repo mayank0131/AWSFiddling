a743a4ff8934abf0d58528dfad070ec6a6d08187
Mayank <dixit.mayank1993@gmail.com>
2025-07-16 07:06:09 +0100

    substituting values with variables

--- ec2-app-hosting-80/vpc.tf
+++ ec2-app-hosting-80/vpc.tf
@@ -2,23 +2,23 @@ data "aws_availability_zones" "zones" {
   state = "available"
 }
 resource "aws_vpc" "app_vpc" {
-  cidr_block = "10.1.0.0/16"
+  cidr_block = var.vpc_cidr
   tags = {
     Name = "bastion-vpc"
   }
 }
 
 resource "aws_subnet" "bastion_subnet" {
-  count                   = 2
+  count                   = var.public_subnet_count
   vpc_id                  = aws_vpc.app_vpc.id
-  cidr_block              = cidrsubnet(aws_vpc.app_vpc.cidr_block, 8, count.index + 1)
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index + 1)
   map_public_ip_on_launch = true
   availability_zone       = data.aws_availability_zones.zones.names[count.index]
 }
 
 resource "aws_subnet" "app_server_subnet" {
-  count             = 2
+  count             = var.private_subnet_count
   vpc_id            = aws_vpc.app_vpc.id
-  cidr_block        = cidrsubnet(aws_vpc.app_vpc.cidr_block, 8, count.index + 3)
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index + 3)
   availability_zone = data.aws_availability_zones.zones.names[count.index]
 }
